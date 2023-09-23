09fc13589e84f5bf5b2a22b96fce1829b04867be
davidwellejus <davidwellejus@gmail.com>
2023-09-23 17:01:59 +0200

    små rettelser på Player-klassen

--- Player.java
+++ Player.java
@@ -7,23 +7,25 @@ public class Player {
         this.name = "";
         this.sum = 0;
     }
-   
+    
+    // Name
     public setName(String name) {
         this.name = name;
     }
     public String getName() {
         return(name);
     }
-
+    
+    // Sum
     public void setSum(int sum) {
         this.sum = sum;
-    }
-    
-    public int getSum() {
-        return (sum);
     }
 
     public void pointSum(int points) {
         this.sum += points;
+    }
+
+    public int getSum() {
+        return (sum);
     }
 }
