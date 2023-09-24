f2e766fb7239597532d1279faa4140f4e3fae6e9
davidwellejus <davidwellejus@gmail.com>
2023-09-24 19:22:07 +0200

    spillerenes navne

---
+++ Game.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Game {
+    public static void main(String[] args) {
+        Scanner scanner = new java.util.Scanner(System.in);
+        System.out.println("What's the name of player1? ");
+        var name1 = scanner.nextLine();
+        var player1 = new Player(name1);
+
+        System.out.println("What's the name of player2? ");
+        var name2 = scanner.nextLine();
+        var player2 = new Player(name2);
+    }
+}
