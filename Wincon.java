a82242d22b1db3388a2b3a2022576408a996bfa8
MarcusDTU <142238430+MarcusDTU@users.noreply.github.com>
2023-09-24 20:27:08 +0200

    Sudden death mangler lidt

--- Wincon.java
+++ Wincon.java
@@ -1,8 +1,11 @@
 class WinCondition{
     public boolean WinnerFound;
-
-    public WinCondition(boolean WinnerFound){
-        this.WinnerFound = WinnerFound;
+    public int PointPlayer1;
+    public int PointPlayer2;
+    public WinCondition(boolean WinnerFound, int PointPlayer1, int PointPlayer2){
+        this.WinnerFound    = WinnerFound;
+        this.PointPlayer1   = PointPlayer1;
+        this.PointPlayer2   = PointPlayer2;
     }
     public boolean winCon(){
         
@@ -11,6 +14,10 @@ class WinCondition{
 
         if(PointPlayer1 > 40 || PointPlayer2 > 40){
             if(PointPlayer1<PointPlayer2){
+                this.WinnerFound = true;
+                return this.WinnerFound;
+            }
+            else if(PointPlayer1>PointPlayer2){
                 this.WinnerFound = true;
                 return this.WinnerFound;
             }
@@ -28,6 +35,17 @@ class WinCondition{
     }
     public void SuddenDeath(){
         while(this.WinnerFound == false){
+            var PointPlayer1 = new getSum();
+            var PointPlayer2 = new getSum();
+
+            if(PointPlayer1 < PointPlayer2){
+                this.WinnerFound = true;
+            }
+            else if( PointPlayer1 > PointPlayer2){
+                this.WinnerFound = true;
+                
+            }
+
             
 
         }
