3bafcb4e5d1d6fe1055e99405097a8f2da1f62ee
MarcusDTU <142238430+MarcusDTU@users.noreply.github.com>
2023-09-24 20:12:44 +0200

    WinCon har fået tilføjet lidt logik

--- Wincon.java
+++ Wincon.java
@@ -3,12 +3,27 @@ class WinCondition{
 
     }
     public boolean winCon(){
+        var PointPlayer1 = new getSum();
+        var PointPlayer2 = new getSum();
 
+        if(PointPlayer1 > 40 || PointPlayer2 > 40){
+            if(PointPlayer1<PointPlayer2){
+                return true;
+            }
+            else if (PointPlayer1 == PointPlayer2){
+                SuddenDeath();
+            }
+        }
+        else{
+            return false;
+        }
 
 
 
     }
     public void SuddenDeath(){
-        
+        while(true){
+            
+        }
     }
 }
