4086d57d199c8f3cebeb0e75f94796e07a8c6aaa
davidwellejus <davidwellejus@gmail.com>
2023-09-24 19:29:36 +0200

    add 2

--- Game.java
+++ Game.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Game {
     public static void main(String[] args) {
         Scanner scanner = new java.util.Scanner(System.in);
+        
+        //Taking the names of players.
         System.out.println("What's the name of player1? ");
         var name1 = scanner.nextLine();
         var player1 = new Player(name1);
@@ -10,5 +12,7 @@ public class Game {
         System.out.println("What's the name of player2? ");
         var name2 = scanner.nextLine();
         var player2 = new Player(name2);
+
+        System.out.println(getName(player1) + getName(player2));
     }
 }
