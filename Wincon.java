499e75d8864a3e8064ed30e14499a9d6c48adf91
MarcusDTU <142238430+MarcusDTU@users.noreply.github.com>
2023-09-25 15:39:34 +0200

    method får pointværdier fra player.getsum

--- Wincon.java
+++ Wincon.java
@@ -2,15 +2,17 @@ class WinCondition{
     public boolean WinnerFound;
     public int PointPlayer1;
     public int PointPlayer2;
+    public String Winner;
     public WinCondition(boolean WinnerFound, int PointPlayer1, int PointPlayer2){
         this.WinnerFound    = WinnerFound;
         this.PointPlayer1   = PointPlayer1;
         this.PointPlayer2   = PointPlayer2;
+       
     }
     public boolean winCon(){
         
-        var PointPlayer1 = new getSum();
-        var PointPlayer2 = new getSum();
+        var PointPlayer1 = new player1.getSum();
+        var PointPlayer2 = new player2.getSum();
 
         if(PointPlayer1 > 40 || PointPlayer2 > 40){
             if(PointPlayer1<PointPlayer2){
@@ -35,8 +37,8 @@ class WinCondition{
     }
     public void SuddenDeath(){
         while(this.WinnerFound == false){
-            var PointPlayer1 = new getSum();
-            var PointPlayer2 = new getSum();
+            var PointPlayer1 = new getDie();
+            var PointPlayer2 = new getDie();
 
             if(PointPlayer1 < PointPlayer2){
                 this.WinnerFound = true;
