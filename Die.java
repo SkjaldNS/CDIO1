4c7aab371f64fcb6bdd9f05470790eb8f56a3af9
MarcusDTU <142238430+MarcusDTU@users.noreply.github.com>
2023-09-25 15:09:41 +0200

    GetDie oprettet og lavet

--- Die.java
+++ Die.java
@@ -1,6 +1,13 @@
 import java.util.Random;
 
 public class Die {
+    public int die1;
+    public int die2;
+    
+    public Die(){
+        this.die1 = rollDie();
+        this.die2 = rollDie();
+    }
     public static void main(String[] args) {
         
         int die1 = rollDie();
@@ -20,5 +27,13 @@ public class Die {
     public static int rollDie() {
         Random rand = new Random();
         return rand.nextInt(6) + 1;
+    }
+    public int[] getDie(){
+        var die = new Die();
+        int dieArray[] = new int[2];
+        for (int element: dieArray){
+            dieArray[element] = rollDie();
+        }
+        return dieArray[];
     }
 }
