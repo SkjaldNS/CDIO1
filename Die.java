87d894ae2333384cc483e149c6847a776a45e5ca
MarcusDTU <142238430+MarcusDTU@users.noreply.github.com>
2023-09-25 15:11:06 +0200

    Main method removed, as no longer needed

--- Die.java
+++ Die.java
@@ -8,21 +8,6 @@ public class Die {
         this.die1 = rollDie();
         this.die2 = rollDie();
     }
-    public static void main(String[] args) {
-        
-        int die1 = rollDie();
-        int die2 = rollDie();
-
-        
-        int sum = die1 + die2;
-
-       
-        System.out.println("Result:");
-        System.out.println("Die 1: " + die1);
-        System.out.println("Die 2: " + die2);
-        System.out.println("Total sum: " + sum);
-    }
-
     
     public static int rollDie() {
         Random rand = new Random();
@@ -34,6 +19,6 @@ public class Die {
         for (int element: dieArray){
             dieArray[element] = rollDie();
         }
-        return dieArray[];
+        return dieArray;
     }
 }
